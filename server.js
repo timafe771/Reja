dce43f53248924c55e5577c3c09dd74035ff639b
unknown <timafe771>
2024-02-06 22:39:16 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web server boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: kirish kodlari
 app.use(express.static("public"));
@@ -28,6 +38,10 @@ app.get("/gift", function (req, res) {
 app.post("/create-item", (req, res) => {
   console.log(req);
   res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
