cc4b02de59a601c62ad8eb028cf685e8682cbce3
unknown <timafe771>
2024-02-10 02:15:51 +0900

    feat: train callback hamda async function

--- train.js
+++ train.js
@@ -1,1 +1,103 @@
-console.log("TRAIN ARIA!");
+console.log("Jack Ma maslahatlari");
+const list = [
+  "Yaxshi talaba boling", // 0-20
+  "togri boshliq tanlang va ko'proq xato qiling", // 20-30
+  "uzingizni ishlaringizni boshlang", //30-40
+  "siz kuchli bolgan narsalarni qiling", // 40-50
+  "yoshlarga investitsiya qiling", //50-60
+  "orqaga chekinmang", // 60-70~~~*/
+];
+// 21 - dars
+/*
+function maslahatlarBering(a, callback) {
+  if (typeof a !== "number") callback("Insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setInterval(function () {
+      callback(null, list[5]);
+    }, 5000);
+  }
+}
+
+console.log("passed here 0");
+maslahatlarBering(65, (err, data) => {
+  if (err) console.log("ERROR:", err);
+  else {
+    console.log("javob:", data);
+  }
+});
+console.log("passed here 1");
+*/
+
+// 22 - dars
+
+function maslahatlarBering(a, callback) {
+  if (typeof a !== "number") callback("Insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setInterval(function () {
+      callback(null, list[5]);
+    }, 1000);
+  }
+}
+
+console.log("passed here 0");
+maslahatlarBering(65, (err, data) => {
+  if (err) console.log("ERROR:", err);
+  else {
+    console.log("javob:", data);
+  }
+});
+console.log("passed here 1");
+
+// async function maslahatlarBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setInterval(() => {
+//         resolve(list[5]);
+//       }, 1000);
+//     });
+//   }
+// }
+// maslahatlarBering(31, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+// console.log("passed here 1");
+
+// then/ catch
+/*
+console.log("passed here 0");
+maslahatlarBering(65)
+  .then((data) => {
+    console.log("javob", data);
+  })
+  .catch((err) => {
+    console.log("ERROR:", err);
+  });
+console.log("passed here 1");*/
+
+// async / await
+// async function run() {
+//   let javob = await maslahatlarBering(65);
+//   console.log(javob);
+//   javob = await maslahatlarBering(70);
+//   console.log(javob);
+//   javob = await maslahatlarBering(41);
+//   console.log(javob);
+// }
+// run();
