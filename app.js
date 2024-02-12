0831171b82e9b244f84e41b0a714c68baa580302
unknown <timafe771>
2024-02-13 05:46:46 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -37,17 +37,36 @@ app.get("/gift", function (req, res) {
 });
 */
 
-// app.post("/create-item", (req, res) => {
-//   console.log(req);
-//   res.json({ test: "success" });
-// });
+app.post("/create-item", (req, res) => {
+  console.log("user entered /create-item");
+  // console.log(req.body);
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
+});
 
 app.get("/author", (req, res) => {
   res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        res.render("reja", { items: data });
+      }
+    });
 });
 
 module.exports = app;
